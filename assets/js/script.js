c2a6752db9ded19dd2bc766442f16a5a73363332
Gdel-J <jessygdelvecchio@gmail.com>
2023-03-17 17:37:05 -0400

    html file set up, Css and Js files started

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+var questionsArr = [
+{
+    question: 'Commonly used data types do not include:',
+    choices: {
+        a:'A. strings',
+        b: 'B. booleans',
+        c:'C.numbers',
+        d:'D. alerts',
+    },
+    answer:'d'
+},
+
+{  quetion:'',
+   choices: {
+    a:'',
+    b:'',
+    c:'',
+    d:'',
+}   ,
+
+answer: ''
+},
+
+{  quetion:'',
+   choices: {
+    a:'',
+    b:'',
+    c:'',
+    d:'',
+}   ,
+
+answer: ''
+},
+
+{  quetion:'',
+   choices: {
+    a:'',
+    b:'',
+    c:'',
+    d:'',
+}   ,
+
+answer: ''
+},
+
+{  quetion:'',
+   choices: {
+    a:'',
+    b:'',
+    c:'',
+    d:'',
+}   ,
+
+answer: ''
+},
+
+];
