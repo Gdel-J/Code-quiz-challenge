af3dbf5df2485caee539d3511f7d7e7a11bab83f
Gdel-J <jessygdelvecchio@gmail.com>
2023-03-17 20:25:18 -0400

    Js methods

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,8 +67,39 @@ var mark = 0;
 var index = 0;
 var record = [];
 
+
+
 // create view high scores using ' createElement' and 'add' methods
 var viewScore = document.createElement('p');
 viewScore.classList.add('banner', 'view-score');
 viewScore.textContent = 'View High Scores';
 
+ // create time using 'appendChild' method
+ var time = document.createElement('p');
+ time.classList.add('banner', 'time');
+ time.textContent = 'Time: ';
+ var second = document.createElement('span');
+ second.setAttribute('id', 'second');
+ time.appendChild(second);
+
+
+ // create container title
+ var title = document.createElement('h1');
+ title.classList.add('title');
+ title.textContent = 'Code quiz challenge';
+
+ // create container text
+ var text = document.createElement('p');
+ text.classList.add('text');
+ text.textContent = 'Try to answer the questions within the time limit. Incorrect answers will remove 10 seconds from the timer and from your score!';
+
+  // create container start btn
+  var startBtn = document.createElement('button');
+  startBtn.classList.add('btn', 'btn-start');
+  startBtn.textContent = 'Start Quiz';
+// With these appendChild method, these arguments: View score, time, title text, and start button are displayed in the browser!
+  header.appendChild(viewScore);
+  header.appendChild(time);
+  container.appendChild(title);
+  container.appendChild(text);
+  container.appendChild(startBtn);
